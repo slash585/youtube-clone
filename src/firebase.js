5da751aeb665b11f24aeaccd825a5688be6c107c
slash585 <51036040+slash585@users.noreply.github.com>
2021-09-19 16:13:59 +0300

    Initializing app

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAyz9yeK625cTGGWuAkA0ZRE8T08B3CmlM',
+  authDomain: 'clone-d9ba1.firebaseapp.com',
+  projectId: 'clone-d9ba1',
+  storageBucket: 'clone-d9ba1.appspot.com',
+  messagingSenderId: '171364942897',
+  appId: '1:171364942897:web:98e23ae935d7aaa9704110',
+  measurementId: 'G-52MK93GPV3',
+}
